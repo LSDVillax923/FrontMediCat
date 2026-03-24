b9f61a2942abdd391901b633e7f6dcae6bef0314
LSD_Villax <andresvillabon@gmail.com>
2026-03-24 17:10:54 -0500

    listar mascotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './inicio/inicio/inicio';
+import { ListarMascotas } from './mascota/listar-mascotas/listar-mascotas';
 
 export const routes: Routes = [
-  { path: '', component: Inicio },
+  { path: '', pathMatch: 'full', redirectTo: 'inicio' },
+  { path: 'inicio', component: Inicio },
+  { path: 'mascotas', component: ListarMascotas },
+  { path: '**', redirectTo: 'inicio' },
 ];
