a5e6b044d1ac828c715429cd391ae4e66fa581ee
LSD_Villax <andresvillabon@gmail.com>
2026-03-26 14:27:24 -0500

    lay out

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
