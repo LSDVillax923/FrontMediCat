626be483d48e6a1c044fd8774559a210e1480045
LSD_Villax <andresvillabon@gmail.com>
2026-04-14 16:48:05 -0500

    ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authTokenInterceptor } from './shared/auth/auth-token.interceptor';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { authTokenInterceptor } from './shared/auth/auth-token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-      provideHttpClient(withInterceptors([authTokenInterceptor])),
+    provideHttpClient(withInterceptors([authTokenInterceptor])),
   ],
 };
